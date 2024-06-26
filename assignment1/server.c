08084e5076cd7c2a591ff30acf366b6963c32fb5
wu-seong <jws001228@naver.com>
2024-06-26 09:51:19 +0000

    hearbeat response 생성 및 전송 과정 추가

--- assignment1/server.c
+++ assignment1/server.c
@@ -27,7 +27,7 @@ typedef struct {
 
 bool check_reason(cJSON* reason){
 	if (cJSON_IsString(reason) && (reason -> valuestring != NULL)) {
-			printf("Reason: %s\n", reason -> valuestring);
+			//printf("Reason: %s\n", reason -> valuestring);
 			return strcmp(reason->valuestring, EXPECTED_TYPE) == 0;
 	}
 }
@@ -46,7 +46,6 @@ char* get_current_time(){
 	char *temp_buffer = (char*)malloc(sizeof(char)*30); 
 	// 날짜와 시간을 포맷팅
     strftime(temp_buffer, 20, "%Y-%m-%dT%H:%M:%S", tm);
-    printf("%s\n", temp_buffer);
     // 밀리초 추가
 	int remaining_size = 30 - strlen(temp_buffer) - 1;
     snprintf(temp_buffer + strlen(temp_buffer), remaining_size, ".%03ldZ", tv.tv_usec / 1000);
@@ -54,7 +53,7 @@ char* get_current_time(){
 }
 
 char* create_boot_notification_response(){
-	cJSON *json_boot_notification_response = cJSON_CreateObject();
+	cJSON* json_boot_notification_response = cJSON_CreateObject();
 	if (json_boot_notification_response == NULL) {
 		printf("Error creating JSON\n");
 		cJSON_Delete(json_boot_notification_response);
@@ -70,6 +69,32 @@ char* create_boot_notification_response(){
 	cJSON_Delete(json_boot_notification_response);
 	return json_string;
 }
+char* create_hearbeat_response(){
+	cJSON* json_heartbeat_response = cJSON_CreateObject();
+	if (json_heartbeat_response == NULL) {
+		printf("Error creating JSON\n");
+		cJSON_Delete(json_heartbeat_response);
+		return NULL;
+	}
+	char* current_time = get_current_time();
+	cJSON_AddStringToObject(json_heartbeat_response, "_type", "HeartbeatResponse");
+	cJSON_AddStringToObject(json_heartbeat_response, "_timestamp", current_time);
+	cJSON_AddStringToObject(json_heartbeat_response, "currentTime", current_time);
+	char* buffer = cJSON_PrintUnformatted(json_heartbeat_response);
+	cJSON_Delete(json_heartbeat_response);
+	return buffer;
+}
+
+bool check_message_type(const char* message, char* expected_type){
+	cJSON* json_message = cJSON_Parse(message);
+	if(json_message == NULL){
+		perror("JSON parse failed");
+	}
+	cJSON* json_type = cJSON_GetObjectItemCaseSensitive(json_message ,"_type");
+	if(cJSON_IsString(json_type) && (json_type->valuestring != NULL)){
+        return strcmp(json_type -> valuestring, expected_type) == 0;
+    }
+}
 
 // 첫 연결 후 실행할 콜백 함수
 void* handle_client(void *arg){
@@ -77,23 +102,21 @@ void* handle_client(void *arg){
 	char read_buffer[BUFFER_SIZE];
 
 	// 요청 메시지 읽기
-	printf("recv wait...");
-	int read_bytes = recv(client->socket, &read_buffer, sizeof(read_buffer), 0);
+	//printf("recv wait...");
+	int read_bytes = recv(client->socket, &read_buffer, BUFFER_SIZE - 1, 0);
 	if (read_bytes > 0) {
     	read_buffer[read_bytes] = '\0'; 
         printf("Server Received: %s\n", read_buffer);
-		cJSON *json = cJSON_Parse(read_buffer);
+		cJSON* json = cJSON_Parse(read_buffer);
 		if(json == NULL){
 			perror("JSON parse failed");
 		}
 		bool is_correct_reason = check_reason(cJSON_GetObjectItemCaseSensitive(json, "reason"));
 		// reason이 PowerUp이 아니라면 클라이언트와의 연결 종료 후 다시 클라이언트 연결 대기
-		printf("isTrue: %s\n", is_correct_reason ? "true" : "false");
 		if(!is_correct_reason){
 			cJSON_Delete(json);
 			close(client->socket);
 			free(arg);
-			pthread_exit(NULL);
 		}
 		// chargin station 식별 ...
 
@@ -102,21 +125,45 @@ void* handle_client(void *arg){
 		// response 만들어서 send
 		// currentTime(String), status(), inerval 정보 만들어서 JSON serialize
 		char* boot_notification_response = create_boot_notification_response();
-		printf("보낼 메시지: %s\n", boot_notification_response);
-		if( write(client->socket, boot_notification_response, strlen(boot_notification_response) ) ){
+		if( write(client->socket, boot_notification_response, strlen(boot_notification_response) ) == -1 ){
 			perror("write failed");
 		}
-		printf("Server Send: %s\n", boot_notification_response);
-
-		// 이후로는 heartbeatRequest에 대한 response 전달
+		printf("Sent Message: %s\n", boot_notification_response);
 	}
 	else if(read_bytes == -1){
 		perror("recv failed");
 	}
 	else if(read_bytes == 0){
 		printf("client disconnected\n");
+		close(client->socket);
+		free(arg);
+		return NULL;
 	}
 
+	// Accept를 보낸 경우 heartbeatRequest 수신 대기
+	while(1){
+		memset(read_buffer, 0, BUFFER_SIZE);
+		read_bytes = recv(client->socket, &read_buffer, BUFFER_SIZE - 1, 0);
+		read_buffer[read_bytes] = '\0';
+		printf("Recived Message: %s\n", read_buffer);
+		if(read_bytes > 0){
+			if(check_message_type(read_buffer, "HeartbeatRequest")){
+				char* heartbeat_response = create_hearbeat_response();
+				if(write(client->socket, heartbeat_response, strlen(heartbeat_response)) == -1){
+					perror("write failed");
+				}
+				printf("Sent Message: %s\n", heartbeat_response);
+			}
+		}
+		else if(read_bytes == -1){
+			perror("recv failed");
+		}
+		else if(read_bytes == 0){
+			printf("client disconnected\n");
+			close(client->socket);
+			free(arg);
+		}
+	}
 	return NULL;
 }
 	
@@ -140,7 +187,7 @@ int main(){
 	if(listen(passive_sock, 10) < 0){
 		perror("listen failed\n");
 	}
-	printf("Listening...");
+	printf("Listening...\n");
 
 	while(1){
 		memset(&sin, 0, sizeof(sin));
@@ -156,7 +203,9 @@ int main(){
         new_client->address = sin;
 
 		// 관리할 소켓 대상에 추가 및 스레드 생성
+		//printf("lock waiting...\n");
 		pthread_mutex_lock(&client_sockets_mutex);
+		//printf("lock acquire\n");
 		for(int i = 0; i< MAX_CLIENTS; i++){
 			if(client_sockets[i] == NULL){
 				client_sockets[i] = new_client;
@@ -170,11 +219,13 @@ int main(){
 					// 자식 스레드 분리
 					pthread_detach(t_id); 
 				}
+				break;
 			}
-			break;
 		}
 		pthread_mutex_unlock(&client_sockets_mutex);
 	
 
 	}
+	close(passive_sock);
+	return 0;
 }
