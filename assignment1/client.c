70fd5960c38e807a500ff648b85ae50559829ac1
wu-seong <jws001228@naver.com>
2024-06-26 09:52:04 +0000

    heartbeat request interval 주기로 보내고 response 받아 currentTime 출력 과정 추가

--- assignment1/client.c
+++ assignment1/client.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,7 +28,6 @@ char* get_current_time(){
 	char *temp_buffer = (char*)malloc(sizeof(char)*30); 
 	// 날짜와 시간을 포맷팅
     strftime(temp_buffer, 20, "%Y-%m-%dT%H:%M:%S", tm);
-    printf("%s\n", temp_buffer);
     // 밀리초 추가
 	int remaining_size = 30 - strlen(temp_buffer) - 1;
     snprintf(temp_buffer + strlen(temp_buffer), remaining_size, ".%03ldZ", tv.tv_usec / 1000);
@@ -79,13 +79,12 @@ char* create_heartbeat_request(){
 
 bool check_status(cJSON* status){
 	if (cJSON_IsString(status) && (status -> valuestring != NULL)) {
-			printf("status: %s\n", status -> valuestring);
+			//printf("status: %s\n", status -> valuestring);
 			return strcmp(status->valuestring, "Accepted") == 0;
 	}
 }
 
 int main() {
-    int interval;
     int sock;
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
@@ -110,14 +109,14 @@ int main() {
         error_handling("write() error");
     }
 
-    printf("Sent JSON: %s\n", boot_notification_request);
+    printf("Sent Message: %s\n", boot_notification_request);
 
     int str_len = read(sock, buffer, BUFFER_SIZE - 1);
     if (str_len == -1) {
         error_handling("read() error");
     }
     buffer[str_len] = '\0';
-    printf("Received response JSON: %s\n", buffer);
+    printf("Received Message: %s\n", buffer);
     cJSON *json_boot_notification_response = cJSON_Parse(buffer);
     if(json_boot_notification_response == NULL){
         perror("JSON parse failed");
@@ -131,14 +130,42 @@ int main() {
     }
 
     cJSON* json_interval = cJSON_GetObjectItemCaseSensitive(json_boot_notification_response, "interval");
-    interval = json_interval->valueint;
+    struct timeval timeout;
+    timeout.tv_sec = json_interval->valueint;
+    timeout.tv_usec = 0;
+    cJSON_Delete(json_interval);
+
     while(1){
         char* heartbeat_request = create_heartbeat_request();   
+        if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout)) < 0) {
+            error_handling("setsockopt() error");
+        }
+
         if (write(sock, heartbeat_request, strlen(heartbeat_request)) == -1) {
             error_handling("write() error");
         }
-        printf("Sent JSON: %s\n", heartbeat_request);
-        sleep(interval);
+        printf("Sent Message: %s\n", heartbeat_request);
+
+        free(heartbeat_request);
+        memset(buffer, 0, BUFFER_SIZE);
+        int str_len = read(sock, buffer, BUFFER_SIZE - 1);
+        if (str_len == -1) { 
+            if (errno == EWOULDBLOCK || errno == EAGAIN) {
+                printf("No response from server within %d seconds, closing connection.\n", 5);
+                close(sock);
+                exit(EXIT_FAILURE);
+            } else {
+                error_handling("read() error");
+            }
+        } else if (str_len == 0) {
+            printf("Server closed the connection.\n");
+        } else {
+            buffer[str_len] = '\0';
+            cJSON* json_heartbeaet_response = cJSON_Parse(buffer);
+            printf("Received message: %s\n", buffer);
+            printf("Server currentTime: %s\n", cJSON_GetObjectItemCaseSensitive(json_heartbeaet_response, "currentTime")->valuestring);
+        }
+        sleep(timeout.tv_sec); 
     }
     return 0;
 }
